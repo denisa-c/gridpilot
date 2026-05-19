a167a906fed4dd17e021a13363480282665eb3e9
denisa-c <denisa.a.constantinescu@gmail.com>
2026-05-19 19:48:14 +0200

    runscript

---
+++ experiments_v2/scripts/run.sh
@@ -0,0 +1,3 @@
+PYTHONPATH=gridpilot/src:gridpilot/experiments_v2/src python3 \
+    gridpilot/experiments_v2/scripts/02_run_country_sweep.py \
+    --countries SE --mw 10 --seeds 1 --workers 1 --max-jobs 5000
